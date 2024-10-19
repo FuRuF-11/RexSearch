37d50f3eb8096938cd5b452da336a6c945a63cff
FuRuF-11 <furuf15@gmail.com>
2024-10-19 19:55:39 +0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import numpy as np
+
+
+
+
+
+
+def SearchTrain():
+    pass
+
+
+def SearchEval():
+    pass
+
+
+def SearchInferenece():
+    pass
+
+def main():
+    # train()
+    SearchTrain()
+    # eval()
+    SearchEval()
+    # inference()
+    SearchInferenece()
+
+if __name__=="__main__":
+    main()
